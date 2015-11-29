1ded4ac4303141404a06d92638a4fe3697bd5fd4
FormatCom <formatcomvinicio@gmail.com>
2015-11-29 16:58:34 -0500

    se implemento logs de access y error, para saber estado del servidor

--- hackNode/index.php
+++ hackNode/index.php
@@ -3,12 +3,10 @@
 	require_once("node.php");
 
   define("NODE_DIR", "/home/formatcom/node");
-  define("NODE_PORT", 49999);
-  define("NODE_PID_DIR", "/home/formatcom/node/pid");
   define("NODE_APP", "/home/formatcom/index.js");
   define("ADMIN_PASS", "12345Admin");
 
-	$Node = new Node(NODE_DIR, NODE_PORT, NODE_PID_DIR, NODE_APP, ADMIN_PASS);
+	$Node = new Node(NODE_DIR, NODE_APP, ADMIN_PASS);
 
   if (isset($_REQUEST["exec"]) && isset($_REQUEST["pass"])){
 
--- hackNode/node.php
+++ hackNode/node.php
@@ -1,71 +1,102 @@
 <?php
 
-define("UNDEFINE", 0);
-define("ERROR", 1);
-define("START", 2);
-define("RUNNING", 3);
-define("NORUNNING", 4);
-define("STOP", 5);
-
 class Node{
 
-  private $NODE_DIR, $NODE_PORT, $NODE_PID_DIR, $NODE_APP, $ADMIN_PASS;
+  //List status.
+  private static $UNDEFINE  = 0;
+  private static $ERROR     = 1;
+  private static $START     = 2;
+  private static $RUNNING   = 3;
+  private static $NORUNNING = 4;
+  private static $STOP      = 5;
 
-  public function Node($NODE_DIR, $NODE_PORT, $NODE_PID_DIR, $NODE_APP, $ADMIN_PASS) {
+  private $NODE_DIR, $NODE_APP, $ADMIN_PASS;
+
+  public function Node($NODE_DIR, $NODE_APP, $ADMIN_PASS) {
     $this->NODE_DIR     = $NODE_DIR; //Carpeta de Nodejs.
-    $this->NODE_PORT    = $NODE_PORT; //Puerto del servidor Nodejs.
-    $this->NODE_PID_DIR = $NODE_PID_DIR; //Carpeta donde se almacena direccion del proceso.
     $this->NODE_APP     = $NODE_APP; //Aplicacion Nodejs.
     $this->ADMIN_PASS   = $ADMIN_PASS; //Password para las peticiones.
   }
 
+  private function writeFile($FILE, $STRING) {
+    $now    = date("d-m-Y H:i:s -> ");
+    $buffer = @fopen("$this->NODE_DIR/supervisor/$FILE", "a+");
+    @fwrite($buffer, $now.$STRING."\r");
+    @fclose($buffer);
+  }
+
   private function report($STATUS) {
-    if ($STATUS === START || $STATUS === RUNNING) $data = array("running" => true, "status" => $STATUS);
+    if ($STATUS === self::$START || $STATUS === self::$RUNNING) $data = array("running" => true, "status" => $STATUS);
     else $data = array("running" => false, "status" => $STATUS);
     return $_GET['callback']."([".json_encode($data)."])";
   }
 
   public function start($PASS) {
 
-    if($PASS !== $this->ADMIN_PASS) return $this->report(ERROR);
+    if(!file_exists($this->NODE_DIR)) return $this->report(self::$UNDEFINE);
 
-    if(!file_exists($this->NODE_DIR)) return $this->report(UNDEFINE);
+    if(!file_exists("$this->NODE_DIR/supervisor")) mkdir("$this->NODE_DIR/supervisor", 0755, true);
 
-  	$node_pid = @intval(file_get_contents("$this->NODE_PID_DIR/nodepid"));
+    if($PASS !== $this->ADMIN_PASS){
+      $this->writeFile("error.log", "ERROR PASSWORD.");
+      return $this->report(self::$ERROR);
+    }
 
-  	if($node_pid > 0) return $this->report(RUNNING);
+  	$node_pid = @intval(file_get_contents("$this->NODE_DIR/supervisor/pid"));
 
-    if(!file_exists($this->NODE_APP)) return $this->report(ERROR);
+    if(file_exists("/proc/$node_pid")) return $this->report(self::$RUNNING);
+
+    if(!file_exists($this->NODE_APP)){
+      $this->writeFile("error.log", "APP UNDEFINE.");
+      return $this->report(self::$ERROR);
+    }
 
   	$file = escapeshellarg($this->NODE_APP);
 
-  	$node_pid = exec("PORT=$this->NODE_PORT $this->NODE_DIR/bin/node $file > /dev/null & echo $!");
+  	$node_pid = exec("$this->NODE_DIR/bin/node $file > /dev/null & echo $!");
 
-    file_put_contents("$this->NODE_PID_DIR/nodepid", $node_pid, LOCK_EX);
+    @file_put_contents("$this->NODE_DIR/supervisor/pid", $node_pid, LOCK_EX);
   	sleep(1); //Espera que se levante Notejs.
 
-    if($node_pid > 0) return $this->report(START);
-    else return $this->report(ERROR);
+    if($node_pid > 0){
+      $this->writeFile("access.log", "APP START IN PID: $node_pid.");
+      return $this->report(self::$START);
+    }else{
+      $this->writeFile("error.log", "ERROR APP START.");
+      return $this->report(self::$ERROR);
+    }
   }
 
   public function stop($PASS) {
 
-    if($PASS !== $this->ADMIN_PASS) return $this->report(ERROR);
+    if(!file_exists($this->NODE_DIR)) return $this->report(self::$UNDEFINE);
 
-    if(!file_exists($this->NODE_DIR)) return $this->report(UNDEFINE);
+    if($PASS !== $this->ADMIN_PASS){
+      $this->writeFile("error.log", "ERROR PASSWORD.");
+      return $this->report(self::$ERROR);
+    }
 
-    $node_pid = @intval(file_get_contents("$this->NODE_PID_DIR/nodepid"));
+    $node_pid = @intval(file_get_contents("$this->NODE_DIR/supervisor/pid"));
 
-  	if($node_pid === 0) return $this->report(NORUNNING);
+  	if($node_pid === 0) return $this->report(self::$NORUNNING);
+
+    if(!file_exists("/proc/$node_pid")){
+      $this->writeFile("error.log", "DOWN APP SERVER IN PID: $node_pid.");
+      @file_put_contents("$this->NODE_DIR/supervisor/pid", '', LOCK_EX);
+      return $this->report(self::$NORUNNING);
+    }
 
   	$ret = -1;
   	passthru("kill $node_pid", $ret);
 
-
     if($ret === 0){
-      file_put_contents("$this->NODE_PID_DIR/nodepid", '', LOCK_EX);
-      return $this->report(STOP);
-    }else return $this->report(ERROR);
+      $this->writeFile("access.log", "APP STOP IN PID: $node_pid.");
+      @file_put_contents("$this->NODE_DIR/supervisor/pid", '', LOCK_EX);
+      return $this->report(self::$STOP);
+    }else{
+      $this->writeFile("error.log", "ERROR STOP APP IN PID: $node_pid.");
+      return $this->report(self::$ERROR);
+    }
   }
 
 }
