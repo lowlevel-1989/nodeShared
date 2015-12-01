00ec105071e24eacd125c905fff1c0b4eb81554d
FormatCom <formatcomvinicio@gmail.com>
2015-12-01 13:29:17 -0500

    se agrego el index.php para el control del daemon admin

---
+++ hackNode/admin/index.php
@@ -0,0 +1,12 @@
+<?php
+  require_once("config.php");
+  if(isset($_POST[pass])){
+    $pass = $_POST[pass];
+    if ($pass === ADMIN_PASS){
+      $response = $DAEMONS["admin"]->start(ADMIN_PASS);
+      //analiza la respuesta si esta corriendo redicciona a la add admin nodejs.
+    }
+  }
+?>
+
+<!-- Aqui dise;a el formulario principal -->
