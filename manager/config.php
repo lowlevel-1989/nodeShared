ae1e013c96b1ffefb956307d4f959540614c216e
FormatCom <formatcomvinicio@gmail.com>
2015-12-03 12:07:30 -0500

    organizando la base para soporte a distintos daemon

--- manager/config.php
+++ manager/config.php
@@ -1,12 +1,13 @@
 <?php
-  require_once("node.php");
-
-  define("NODE_DIR", "/home/formatcom/node");
+  require_once("core/node.php");
   define("ADMIN_PASS", "12345Admin");
+
+  $PATH = Array(
+    "node" => "/home/formatcom/node"
+  );
 
   $DAEMONS = Array(
     "admin" => new Node("Admin", NODE_DIR, "panel/server/index.js", ADMIN_PASS),
     "app"   => new Node("App", NODE_DIR, "/home/formatcom/index.js", ADMIN_PASS)
   );
-
  ?>
--- manager/routes.php
+++ manager/routes.php
@@ -1,5 +1,12 @@
+<?php
+  $URL = Array(
+    "app" => ["^node/(.+?)", "http://127.0.0.1:49999/$1"]
+  );
+?>
+<!--
 Options -Indexes
 
 RewriteEngine On
 RewriteRule ^node(.*?)$  http://127.0.0.1:49998/$1 [P] # http://tudomio.com/node  -> panel del admin.
 RewriteRule ^node/(.*?)$ http://127.0.0.1:49999/$1 [P] # http://tudomio.com/node/ -> app por defecto.
+ -->
