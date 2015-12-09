e64f0db3367233d9c34a4d47b20a38a61cf4a824
FormatCom <formatcomvinicio@gmail.com>
2015-12-09 10:09:25 -0500

    se elimino linea de index

--- manager/core/access.php
+++ manager/core/access.php
@@ -1,7 +1,6 @@
 <?php
   function writeHtaccess($ACCESS) {
     $buffer = fopen('../.htaccess', 'w+');
-    fwrite($buffer, "Options -Indexes\r");
     fwrite($buffer, "RewriteEngine On\r");
     foreach ($ACCESS as $key => $value) {
       fwrite($buffer, "RewriteRule $value[0] $value[1] [P]\r");
