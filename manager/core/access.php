ff0150ddc200770d5b927013a0708f46231f5a4e
FormatCom <formatcomvinicio@gmail.com>
2015-12-09 10:23:57 -0500

    espero posible solucion

--- manager/core/access.php
+++ manager/core/access.php
@@ -1,9 +1,10 @@
 <?php
   function writeHtaccess($ACCESS) {
     $buffer = fopen('../.htaccess', 'w+');
-    fwrite($buffer, "RewriteEngine On\r");
+    fwrite($buffer, "Options -Indexes\n\n");
+    fwrite($buffer, "RewriteEngine On\n");
     foreach ($ACCESS as $key => $value) {
-      fwrite($buffer, "RewriteRule $value[0] $value[1] [P]\r");
+      fwrite($buffer, "RewriteRule $value[0] $value[1] [P]\n");
     }
     fclose($buffer);
   }
