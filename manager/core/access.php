88d1b67c86592359230c44f2af4cbf0db6ffb8e2
FormatCom <formatcomvinicio@gmail.com>
2015-12-09 09:45:29 -0500

    testiando error 500

--- manager/core/access.php
+++ manager/core/access.php
@@ -1,11 +1,11 @@
 <?php
   function writeHtaccess($ACCESS) {
-    $buffer = fopen('.htaccess', 'w+');
+    $buffer = fopen('../.htaccess', 'w+');
     fwrite($buffer, "Options -Indexes\r\r");
-    fwrite($buffer, "RewriteEngine On\r");
-    foreach ($ACCESS as $key => $value) {
-      fwrite($buffer, "RewriteRule $value[0] $value[1] [P]\r");
-    }
+    // fwrite($buffer, "RewriteEngine On\r");
+    // foreach ($ACCESS as $key => $value) {
+      // fwrite($buffer, "RewriteRule $value[0] $value[1] [P]\r");
+    // }
     fclose($buffer);
   }
 ?>
