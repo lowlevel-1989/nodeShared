1df8f5f4e3c47cdcc934fa457edeee722197d481
FormatCom <formatcomvinicio@gmail.com>
2015-12-09 09:26:37 -0500

    haciendo pruebas con htaccess

--- manager/core/access.php
+++ manager/core/access.php
@@ -1,11 +1,11 @@
 <?php
   function writeHtaccess($ACCESS) {
-    $buffer = fopen(".htaccess", 'a+');
-    @fwrite($buffer, "Options -Indexes\r\r");
-    @fwrite($buffer, "RewriteEngine On\r");
+    $buffer = fopen("htaccess", 'a+');
+    fwrite($buffer, "Options -Indexes\r\r");
+    fwrite($buffer, "RewriteEngine On\r");
     foreach ($ACCESS as $key => $value) {
-      @fwrite($buffer, "RewriteRule $value[0] $value[1] [P]\r");
+      fwrite($buffer, "RewriteRule $value[0] $value[1] [P]\r");
     }
-    @fclose($buffer);
+    fclose($buffer);
   }
 ?>
