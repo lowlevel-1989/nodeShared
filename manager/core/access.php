e39f479e2d9896cdcc0d757025f00f25df67557a
FormatCom <formatcomvinicio@gmail.com>
2015-12-09 10:07:31 -0500

    porque el error?

--- manager/core/access.php
+++ manager/core/access.php
@@ -1,7 +1,7 @@
 <?php
   function writeHtaccess($ACCESS) {
-    $buffer = fopen('../.htaccess', 'a+');
-    fwrite($buffer, "Options -Indexes\r\r");
+    $buffer = fopen('../.htaccess', 'w+');
+    fwrite($buffer, "Options -Indexes\r");
     fwrite($buffer, "RewriteEngine On\r");
     foreach ($ACCESS as $key => $value) {
       fwrite($buffer, "RewriteRule $value[0] $value[1] [P]\r");
