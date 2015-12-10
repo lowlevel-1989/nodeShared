93e4576de7918c30ddae9615882f4eb9d729aa97
FormatCom <formatcomvinicio@gmail.com>
2015-12-09 19:27:59 -0500

    se agrego ajax al panel

--- manager/panel/index.php
+++ manager/panel/index.php
@@ -3,11 +3,32 @@
   if(isset($_POST[pass])){
     $pass = $_POST[pass];
     if ($pass === getenv('NODE_PASS')){
-      // $response = $DAEMONS["admin"]->start(ADMIN_PASS); cambiar por ajax
-      //analiza la respuesta si esta corriendo redicciona a la app admin nodejs.
+?>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('#login').on('submit', function(event) {
+        var data = $(this).serialize();
+        $.ajax({
+          type : 'POST',
+          data : data,
+          dataType: 'jsonp',
+          success : function(response) {
+            console.log(response);
+          }
+        });
+        event.preventDefault();
+      });
+    });
+  </script>
+<?php
     }
   }
-  echo getenv('NODE_PASS');
 ?>
 
-<!-- Aqui dise;a el formulario principal -->
+<form method="post" id="login">
+  <input type="hidden"   name="daemon" value="admin" />
+  <input type="hidden"   name="exec"   value="start" />
+  <input type="password" name="pass" />
+  <input type="submit" />
+</form>
--- manager/public.php
+++ manager/public.php
@@ -1,6 +1,7 @@
 <?php
   require_once('core/access.php');
   writeHtaccess(Array(
-    'app' => ['^node/(.*?)$', 'http://127.0.0.1:49999/$1']
+    'admin' => ['^admin/(.*?)$', 'http://127.0.0.1:49998/$1'],
+    'app'   => ['^node/(.*?)$',  'http://127.0.0.1:49999/$1']
   ));
 ?>
