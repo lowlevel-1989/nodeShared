94e5b7068f399041af0da10aaff1f2724b3a3ab0
FormatCom <formatcomvinicio@gmail.com>
2015-12-09 19:38:05 -0500

    se reescribio el ajax

--- manager/panel/index.php
+++ manager/panel/index.php
@@ -7,17 +7,18 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
   <script>
     $(document).ready(function() {
-      $('#login').on('submit', function(event) {
-        var data = $(this).serialize();
-        $.ajax({
-          type : 'POST',
-          data : data,
-          dataType: 'jsonp',
-          success : function(response) {
-            console.log(response);
-          }
-        });
-        event.preventDefault();
+      var data = {
+        "daemon": "admin",
+        "exec": "start",
+        "pass": <?php echo $pass; ?>
+      };
+      $.ajax({
+        type : 'POST',
+        data : data,
+        dataType: 'jsonp',
+        success : function(response) {
+          console.log(response);
+        }
       });
     });
   </script>
@@ -27,8 +28,6 @@
 ?>
 
 <form method="post" id="login">
-  <input type="hidden"   name="daemon" value="admin" />
-  <input type="hidden"   name="exec"   value="start" />
   <input type="password" name="pass" />
   <input type="submit" />
 </form>
