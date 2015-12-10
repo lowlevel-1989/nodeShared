1b40ec7641336dedd89d8cef8bd274870e9e50a8
FormatCom <formatcomvinicio@gmail.com>
2015-12-09 20:02:17 -0500

    se agrego server admin

---
+++ manager/panel/server/index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Zona Administrativa!\n');
+  res.write('<a href="manager/?daemon=admin&exec=stop&pass=12345Admin">Salir</a>');
+  res.end();
+}).listen(49998, '127.0.0.1');
