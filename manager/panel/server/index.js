e0450e19221c306122b7b804184d67a4e6184b28
FormatCom <formatcomvinicio@gmail.com>
2015-12-09 20:32:26 -0500

    se agrego ejemplo de zona admin

--- manager/panel/server/index.js
+++ manager/panel/server/index.js
@@ -1,7 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
+  // Ejemplo de zona administrativa, NOTA: ES UN EJEMPLO.
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Zona Administrativa!\n');
-  res.write('<a href="manager/?daemon=admin&exec=stop&pass=12345Admin">Salir</a>');
+  res.write('<a href="../manager/?daemon=admin&exec=stop&pass=12345Admin">Salir</a>');
   res.end();
 }).listen(49998, '127.0.0.1');
