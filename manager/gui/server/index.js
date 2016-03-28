8dd8c24a227b683eeae0768b41df72f7fb8ee79a
FormatCom <formatcomvinicio@gmail.com>
2016-03-27 23:25:41 -0500

    se elimino gui para hacerlo externo

--- manager/gui/server/index.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  // Ejemplo de zona administrativa, NOTA: ES UN EJEMPLO.
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Zona Administrativa!\n');
-  res.write('<a href="/manager/?daemon=admin&exec=stop&key=12345Admin">Salir</a>');
-  res.end();
-}).listen(process.env.PORT || 49998, '127.0.0.1');
