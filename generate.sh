fa6bf93fd3bc0f09a7e81a5fd578c072b0cb5674
FormatCom <formatcomvinicio@gmail.com>
2016-04-06 22:08:14 -0500

    se soluciono error entre versiones

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 rm gh-pages -rf
 mkdir gh-pages
 cd manage
-tar -czvf ../gh-pages/core.tar.gz core
 sh core/update.sh > /dev/null
 cp update core
 mv update ../gh-pages
+tar -czvf ../gh-pages/core.tar.gz core
