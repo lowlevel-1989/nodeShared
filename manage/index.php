d3f27aede4e56be4accf47a0ba0efcd120d19471
Vinicio Valbuena <formatcomvinicio@gmail.com>
2017-01-07 12:10:10 -0500

    se documento el archivo index.php

--- manage/index.php
+++ manage/index.php
@@ -1,7 +1,14 @@
 <?php
   require_once('core/env.php');
+
+  //se asigna una variable de entorno
+  //con la ruta de nodeShared
   ENV::set('NODE_PUBLIC', dirname(__FILE__));
+
+  //verificamos args enviados desde
+  //la terminal
   if (isset($argv[1]) and isset($argv[2])){
+    //nos movemos a la carpeta del proyecto	  
     chdir(dirname(__FILE__));
   }
   require_once('core/api.php');
