7edc7dbb98acc072b82d266225eda0a77993fe0d
Vinicio Valbuena <formatcomvinicio@gmail.com>
2017-01-08 20:30:36 -0500

    se documento el api

--- manage/core/api.php
+++ manage/core/api.php
@@ -5,8 +5,12 @@
   header("Access-Control-Allow-Origin: ".getenv('NODE_ACCESS'));
   header('Content-Type: application/json; charset=utf8');
 
+  // flag se activa si los
+  // args son validos
   $_active = false;
 
+  // verifica si el request
+  // es GET o POST
   if (isset($_REQUEST['exec']) and isset($_REQUEST['daemon'])){
     $exec = strtolower($_REQUEST['exec']);
     $name = $_REQUEST['daemon'];
@@ -14,6 +18,8 @@
     $_active = true;
   }
 
+  // verifica si el request
+  // viene desde la shell
   if (isset($argv[1]) and isset($argv[2]) and getenv('NODE_SHELL_SUPPORT')){
     $exec = strtolower($argv[2]);
     $name = $argv[1];
@@ -21,28 +27,46 @@
     $_active = true;
   }
 
+  // verifica si todos los
+  // args fueron validos
   if ($_active){
 
     $admin_active = getenv('NODE_ADMIN');
     $admin_pass   = getenv('NODE_ADMIN_PASS');
 
+    // verifica si esta activo
+    // el modo admin
     if ($admin_active){
 
+      // lee la version local de nodeShared
       $version     = @file_get_contents('core/update');
+      // lee la ultima version de nodeShared
+      // en el servidor
       $new_version = @file_get_contents('https://formatcom.github.io/nodeShared/update');
 
+      // verifica si tenemos la 
+      // ultima version instalada
       if ($new_version > $version){
+
+	// crea el daemon update
+	// es el encargado de actualizar
+	// nodeShared
         $DAEMON['update'] = new Node('update', $admin_pass, '.', Array(), 'dos2unix core/update.sh && sh core/update.sh core', 1, false, true);
       }else if ($name === 'update'){
         die('You have the most recent version.');
       }
     }
 
+    // se verifica si el daemon no existe
     if(!isset($DAEMON[$name])){
+      
+      // se reporta el error
       $data = Array('running' => false, 'state' => 0);
       die(json_encode($data));
     }
 
+    // se ejecuta la accion selecciona
+    // por el usuario
     switch ($exec) {
       case 'start':
         echo $DAEMON[$name]->start($key);
